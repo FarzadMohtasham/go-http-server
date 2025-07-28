4ab0e1ab19ad0a87355cbc258d4aef572abcd987
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2025-07-28 19:45:03 +0330

    Update

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
@@ -8,12 +9,37 @@ type api struct {
 	addr string
 }
 
+var users = []User{}
+
 func (s *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Users list: ..."))
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(users)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func (s *api) createUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Creating a new user"))
+	var payload User
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	u := User{
+		Name:  payload.Name,
+		Age:   payload.Age,
+		Email: payload.Email,
+	}
+
+	users = append(users, u)
+
+	w.WriteHeader(http.StatusCreated)
 }
 
 func main() {
