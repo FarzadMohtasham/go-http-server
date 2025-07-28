3810713673923921b86b93c4b63a3d073a16946b
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2025-07-28 16:02:56 +0330

    Added basic routing

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+type server struct {
+	addr string
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		switch r.URL.Path {
+		case "/":
+			w.Write([]byte("This is the root path!"))
+			return
+		case "/home":
+			w.Write([]byte("This is the home path!"))
+			return
+		default:
+			w.Write([]byte("Not valid path!!!"))
+			return
+		}
+	}
+}
+
 func main() {
-	println("Hello GO HTTP Server")
+	s := &server{addr: ":8880"}
+	if err := http.ListenAndServe(s.addr, s); err != nil {
+		log.Fatal(err)
+	}
 }
